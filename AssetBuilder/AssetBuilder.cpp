3cf090947598173cd65aa3eb7d2a710e4fc4010f
clemensu42 <68461923+ClemensU42@users.noreply.github.com>
2022-11-25 18:32:28 +0100

    Finished writing .asset file

--- AssetBuilder/AssetBuilder.cpp
+++ AssetBuilder/AssetBuilder.cpp
@@ -5,9 +5,11 @@
 #include <vector>
 #include <iostream>
 #include <filesystem>
+#include <fstream>
 
 #define MAGIC_NUMBER 0x61736574
 
+#pragma pack(push, 1)
 struct assetHeader{
 	uint64_t magicNum;
 	uint64_t filesize;
@@ -15,21 +17,28 @@ struct assetHeader{
 	uint64_t dataListSize;
 	uint32_t assetListEntryAmount;
 };
+#pragma pack(pop)
 
+#pragma pack(push, 1)
 struct assetEntry{
 	uint16_t entrySize;
 	uint64_t dataSize;
 	uint64_t dataPointer;
-	std::string identifier;
+	std::string* identifier;
 };
+#pragma pack(pop)
 
+#pragma pack(push, 1)
 struct assetList{
 	assetEntry* entries;
 };
+#pragma pack(pop)
 
+#pragma pack(push, 1)
 struct dataList{
 	uint8_t* data;
 };
+#pragma pack(pop)
 
 std::vector<std::string> files;
 
@@ -43,6 +52,13 @@ void collectFilesFromDir(std::string dir){
 	}
 }
 
+void writeEntryToFile(std::fstream* stream, assetEntry* entry){
+	stream->write(reinterpret_cast<char*>(&entry->entrySize), 2);
+	stream->write(reinterpret_cast<char*>(&entry->dataSize), 4);
+	stream->write(reinterpret_cast<char*>(&entry->dataPointer), 4);
+	stream->write(entry->identifier->c_str(), entry->identifier->size());
+}
+
 // argv[1] is the output file
 // argv[2] is the asset directory
 int main(int argc, const char** argv){
@@ -51,9 +67,9 @@ int main(int argc, const char** argv){
 		return -1;
 	}
 
-	assetHeader header;
-	assetList assets;
-	dataList data;
+	assetHeader header{};
+	assetList assets{};
+	dataList data{};
 
 	std::cout << "Collecting files..." << std::endl;
 	std::string path = argv[2];
@@ -61,11 +77,10 @@ int main(int argc, const char** argv){
 
 	std::cout << "Creating entries..." << std::endl;
 	assets.entries = (assetEntry*)malloc(files.size() * sizeof(assetEntry));
-	int i = 0;
-	for(const std::string file : files){
-		assets.entries[i].dataSize = std::filesystem::file_size(file);
-		assets.entries[i].identifier = file;
-		assets.entries[i].entrySize = sizeof(uint16_t) + sizeof(uint64_t) * 2 + file.size();
+	for(int i = 0; i < files.size(); i++){
+		assets.entries[i].dataSize = std::filesystem::file_size(files[i]);
+		assets.entries[i].identifier = &files[i];
+		assets.entries[i].entrySize = sizeof(uint16_t) + sizeof(uint64_t) * 2 + files[i].size();
 		i++;
 	}
 
@@ -78,10 +93,39 @@ int main(int argc, const char** argv){
 	}
 	header.filesize = sizeof(assetHeader) + header.dataListSize + header.assetListSize;
 
-	std::cout << "Collecting Data" << std::endl;
+	std::cout << "Collecting Data..." << std::endl;
+	data.data = (uint8_t*)malloc(header.dataListSize);
+	uint64_t dataPos = 0;
+	for(int j = 0; j < header.assetListEntryAmount; j++){
+		assets.entries[j].dataPointer = dataPos;
+		std::ifstream ifs = std::ifstream(*assets.entries[j].identifier, std::ios::binary);
+		if(ifs.is_open()){
+			uint8_t c;
+			c = ifs.get();
+			while(!ifs.eof()){
+				data.data[dataPos++] = c;
+				c = ifs.get();
+			}
+			ifs.close();
+		} else {
+			std::cout << assets.entries[j].identifier << " could not be opened!!! Aborting..." << std::endl;
+			free(assets.entries);
+			free(data.data);
+			return -1;
+		}
+	}
 
+	std::cout << "Creating asset file..." << std::endl;
+	std::fstream assetFile(argv[1], std::ios::out);
+	assetFile.write(reinterpret_cast<char*>(&header), sizeof(header));
+	for(int j = 0; j < header.assetListEntryAmount; j++){
+		writeEntryToFile(&assetFile, &assets.entries[j]);
+	}
+	assetFile.write(reinterpret_cast<char*>(data.data), header.dataListSize);
+	assetFile.close();
 
-
+	std::cout << "Done!\nCreated asset file at: " << argv[1] << std::endl;
 	free(assets.entries);
+	free(data.data);
 	return 0;
 }
